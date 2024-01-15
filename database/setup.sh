5a85d41d564598b6bd43d39ee955d45a7cfab458
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2024-01-15 15:20:40 +0100

    Update setup.sh

--- database/setup.sh
+++ database/setup.sh
@@ -1,1 +1,1 @@
-psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE *.* TO '$POSTGRES_USER'@'%';"
+psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE *.* TO '$USER_NAME'@'%';"
